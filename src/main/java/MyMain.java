9f1bcd479d77d3348e81c382130c0439fc95b126
yuvro <23ymultani@athenian.org>
2021-09-07 21:00:43 -0700

    math bot

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -4,18 +4,29 @@ public class MyMain {
 
     // Calculates the median of the three inputs
     public static int median(int a, int b, int c) {
-        return 0; // REPLACE WITH YOUR CODE
+        int max_num = Math.max(Math.max(a,b), c);
+        int min_num = Math.min(Math.min(a,b), c);
+        int median_num = a + b + c - max_num - min_num;
+        return median_num;
+
     }
 
     // Returns the input with the larger absolute value
     public static int magnitude(int a, int b) {
-        return 0; // REPLACE WITH YOUR CODE
+        if (Math.abs(a) > Math.abs(b)) {
+            return a;
+        }
+        else{
+            return b;
+        }
     }
 
     // Returns the "c" value from the Pythagorean theorem "a^2 + b^2 = c^2",
     // where "a" and "b" are the inputs to the method
     public static double pythagoras(int a, int b) {
-        return 0.0; // REPLACE WITH YOUR CODE
+        int squares = (a*a) + (b*b);
+        double c = Math.sqrt(squares);
+        return c;
     }
 
     public static void main(String[] args) {
@@ -25,6 +36,42 @@ public class MyMain {
         System.out.println(pythagoras(3, 4)); // should be 5.0
 
         Scanner scan = new Scanner(System.in);
-        // YOUR CODE HERE
+        System.out.println("Enter which method you would like to use ");
+        String method_checker = scan.nextLine();
+        String median = "median";
+        String magnitude = "magnitude";
+        String pythagoras = "pythagoras";
+        if (method_checker.equals(median)){
+            Scanner num1 = new Scanner(System.in);
+            System.out.println("Enter the first number: ");
+            int a_num = scan.nextInt();
+            Scanner num2 = new Scanner(System.in);
+            System.out.println("Enter the second numner: ");
+            int b_num = scan.nextInt();
+            Scanner num3 = new Scanner(System.in);
+            System.out.println("Enter the third number: ");
+            int c_num = scan.nextInt();
+            System.out.println(median(a_num,b_num,c_num));
+
+        }
+        if (method_checker.equals(magnitude)){
+            Scanner num1 = new Scanner(System.in);
+            System.out.println("Enter the first number: ");
+            int a_num = scan.nextInt();
+            Scanner num2 = new Scanner(System.in);
+            System.out.println("Enter the second numner: ");
+            int b_num = scan.nextInt();
+            System.out.println(magnitude(a_num, b_num));
+        }
+        if (method_checker.equals(pythagoras)){
+            Scanner num1 = new Scanner(System.in);
+            System.out.println("Enter the first number: ");
+            int a_num = scan.nextInt();
+            Scanner num2 = new Scanner(System.in);
+            System.out.println("Enter the second numner: ");
+            int b_num = scan.nextInt();
+            System.out.println(pythagoras(a_num,b_num));
+        }
+
     }
 }
